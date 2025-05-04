bb76e4a09d9a333afb550be976d8e8ec918233d0
cpeter66 <cpeter66@charlotte.edu>
2025-05-04 18:01:55 -0400

    push

---
+++ itis3135/project/photographyclubproject/javascripts/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(600, 400);
+    background(240);
+    textSize(64);
+    textAlign(CENTER, CENTER);
+    fill(34, 139, 34);
+    text('UNCC', width / 2, height / 2);
+  }
+  
+  function draw() {
+    // Could add animation or interaction here if desired
+  }
+  
